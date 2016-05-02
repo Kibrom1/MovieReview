2e5e9f92000050f9ea807ab9ddf7c591789e0a70
kb <kb@kb-pc>
2016-05-02 15:53:43 -0500

    DAO modified

--- src/main/java/mum/edu/cs544/Model/DirectorDAO.java
+++ src/main/java/mum/edu/cs544/Model/DirectorDAO.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 
 public class DirectorDAO {
 
-	private SessionFactory sessf;
+	private SessionFactory sf;
 
 	public DirectorDAO(SessionFactory sf) {
 		this.sf = sf;
